4a276dc24acbe2fbfd9f981c3fe041048b9b1ab0
Sagnik Pal <sagnikpal@umass.edu>
2026-07-01 20:42:28 -0400

    allow CORS

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
